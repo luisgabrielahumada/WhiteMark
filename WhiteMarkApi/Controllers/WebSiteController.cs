b559e7fb4c12446565d7a4de245627c05147b9bf
luisgabrielahumada <luisgabrielahumada@gmail.com>
2019-03-17 13:24:31 -0500

    Initial commit

---
+++ WhiteMarkApi/Controllers/WebSiteController.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WhiteMarkApi.Helper;
+
+namespace WhiteMarkApi.Controllers
+{
+    public class WebSiteController : ApiController
+    {
+        [HttpGet()]
+        [SwaggerOperation("MarkWebSite")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        public HttpResponseMessage Mark(string token)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new{ data  = HttpWebHtml.GetResponseString("http://www.google.com") });
+        }
+    }
+}
